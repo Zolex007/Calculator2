6eceb8a7c781f4d683880220cf5432b990bad305
Zolex <israelbrunogd@gmail.com>
2026-09-18 19:39:47 -0300

    First commit!

---
+++ calculator.py
@@ -0,0 +1,7 @@
+number_1 = float(input('Type the first number: '))
+number_2 = float(input('Type the second number: '))
+print(f'{number_1} + {number_2} = {number_1 + number_2}')
+print(f'{number_1} - {number_2} = {number_1 - number_2}')
+print(f'{number_1} × {number_2} = {number_1 * number_2}')
+print(f'{number_1} ÷ {number_2} = {number_1 / number_2}')
+# My first translation!!!
